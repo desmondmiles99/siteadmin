56576d9b5176827a4fedcbb931993f21836a5839
desmondmiles99 <55878433+desmondmiles99@users.noreply.github.com>
2021-05-26 17:27:50 +0200

    Add files via upload

--- credentials.js
+++ credentials.js
@@ -8,11 +8,7 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "patrick" && password === "admin") {
-        alert("You have successfully logged in.");
-location.href = "site1.html"}
-else if (username === "admin" && password === "admin") {
-    alert("You have successfully logged in.");
-location.href = "headadmin.html"
+    if (username === "member1" && password === "member") {
+        location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
     }
 })
