743d1b3a285351f8499a574cd3608d9b154a39dc
desmondmiles99 <55878433+desmondmiles99@users.noreply.github.com>
2022-03-19 19:24:00 +0100

    Update credentials.js

--- credentials.js
+++ credentials.js
@@ -10,7 +10,7 @@ loginButton.addEventListener("click", (e) => {
 
     if (username ==="admin" && password === "admin")
     {
-      <a  location.href = "\level2.html">pong</a>
+      <a  location.href = "\level2.html">level2.html</a>
     } else if (username ==="admin1" && password === "admin2") {
         location.href = "591416926886953009.html"
     }
